53ae852fd1a74c7e6ac7dfe731664c28225cb2a3
triann <trian12345@hotmail.com>
2025-08-19 05:57:52 -0300

    favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import type React from "react"
 import type { Metadata } from 'next'
 import { GeistSans } from 'geist/font/sans'
 import { GeistMono } from 'geist/font/mono'
@@ -7,6 +8,9 @@ export const metadata: Metadata = {
   title: 'Loja Oficial LEGO® BR',
   description: 'Lego Store',
   generator: 'clona nao bb',
+  icons: {
+    icon: "https://legobrasil.vtexassets.com/assets/vtex/assets-builder/legobrasil.dup-template/1.28.0/logo___40c43ea8a6afef0f36be240072a0e00d.png",
+  },
 }
 
 export default function RootLayout({
