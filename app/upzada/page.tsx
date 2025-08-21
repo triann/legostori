9ff6bea2525d1c03756d3b50215c0e3bcc26441a
triann <trian12345@hotmail.com>
2025-08-21 13:10:40 -0300

    up1 v2

--- app/upzada/page.tsx
+++ app/upzada/page.tsx
@@ -1,9 +1,15 @@
+import { Header } from "@/components/header"
+import { Footer } from "@/components/footer"
 import UpsellFlow from "@/components/upsell-flow"
 
 export default function UpsellPage() {
   return (
-    <div className="min-h-screen bg-gray-50 py-8">
-      <UpsellFlow />
+    <div className="min-h-screen bg-white">
+      <Header />
+      <div className="bg-gray-50 py-8">
+        <UpsellFlow />
+      </div>
+      <Footer />
     </div>
   )
 }
