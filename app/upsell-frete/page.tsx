e8a6b61a8b651f79ea00c6dcbd12a1c5a4da7310
triann <trian12345@hotmail.com>
2025-08-24 21:28:51 -0300

    vamove2

--- app/upsell-frete/page.tsx
+++ app/upsell-frete/page.tsx
@@ -1,4 +1,3 @@
-import { Suspense } from "react"
 import { Header } from "@/components/header"
 import { Footer } from "@/components/footer"
 import UpsellFreteFlow from "@/components/upsell-frete-flow"
@@ -6,9 +5,7 @@ import UpsellFreteFlow from "@/components/upsell-frete-flow"
 export default function UpsellFretePage() {
   return (
     <div className="min-h-screen bg-white">
-      <Suspense fallback={<div className="h-16 bg-yellow-400"></div>}>
-        <Header />
-      </Suspense>
+      <Header />
       <main className="py-8">
         <UpsellFreteFlow />
       </main>
